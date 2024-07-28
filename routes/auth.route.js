9e05b2541e448ebeaf3efa4b5d42cb8b823cebd5
Harry MacCode <emarh.harry.code@gmail.com>
2024-07-28 11:38:48 +0200

    User validators

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { signup } = require('../controllers/auth.controller');
+const { userSignupValidator } = require('../validation/auth.validation');
+const { runValidation } = require('../validation/index.validation');
 const authRoute = express.Router();
 
-authRoute.post('/signup', signup);
+authRoute.post('/signup', userSignupValidator, runValidation, signup);
 
 module.exports = authRoute;
