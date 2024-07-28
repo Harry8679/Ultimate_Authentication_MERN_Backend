9e02cae4a99d80c414971880531b377fdfff8361
Harry MacCode <emarh.harry.code@gmail.com>
2024-07-28 10:48:21 +0200

    user controller and auth route updated

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 
 exports.signup = asyncHandler((req, res) => {
+    console.log('REQ BODY ON SIGNUP', req.body);
     res.json({
         data: 'You hit signup endpoint'
     });
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,6 +2,6 @@ const express = require('express');
 const { signup } = require('../controllers/auth.controller');
 const authRoute = express.Router();
 
-authRoute.get('/signup', signup);
+authRoute.post('/signup', signup);
 
 module.exports = authRoute;
