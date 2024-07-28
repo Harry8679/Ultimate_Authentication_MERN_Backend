d032f04acda0f28db8ea74af384f748ee1779ff2
Harry MacCode <emarh.harry.code@gmail.com>
2024-07-28 10:13:55 +0200

    Applying Middlewares

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
 const authRoute = require('./routes/auth.route');
+
 dotenv.config();
 
+const app = express();
 const port = process.env.PORT || 6501;
 
+// app middlewares
+app.use(morgan('dev'));
+// app.use(cors());
+if (process.env.NODE_ENV !== 'development') {
+    app.use({ origin: `${process.env.CLIENT_URL}` });
+};
+app.use(express.json()); // To parse JSON bodies
+
+// Import Routes
 app.use('/api/v1/users', authRoute);
 
 app.listen(port, () => {
